6fb91f67fef718a56ca9c78022ba248b4cde270f
Abiola Oyewale <abiol4001@gmail.com>
2022-06-18 12:58:09 +0100

    First commit

---
+++ env/abi0la/abi0la/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for abi0la project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abi0la.settings')
+
+application = get_asgi_application()
